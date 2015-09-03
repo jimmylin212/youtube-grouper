e2db1bc53a145c72293645f78f0a4c17b8074b9f
jimmylin212 <jimmylin212@gmail.com>
2015-09-04 01:33:54 +0800

    Query Userinfo data
    able to query and get the userinfo data.

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,17 +1,25 @@
 import json, os, urllib, httplib, urllib2
-from django import http
-from django.shortcuts import render_to_response
 
-class AuthConfig:
-	scope = r'https://www.googleapis.com/auth/youtube'
-	auth_url_base = r'https://accounts.google.com/o/oauth2/auth'
-	exchange_request_url = r'accounts.google.com/o/oauth2/token'
-	client_secret_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'client_secret.json')
+from django import http
+from django.shortcuts import render_to_response, redirect
+
+from google.appengine.api import oauth
 
 class Youtube_API:
-	base_url = r'https://www.googleapis.com/youtube/v3'
+	scope = 'https://www.googleapis.com/auth/youtube'
+	base_url = 'https://www.googleapis.com/youtube/v3'
 	subscriptions_url = '%s/subscriptions/part=snippet&mine=true' % base_url
 	channel_url = '%s/channels/?part=snippet&mine=true' % base_url
+
+class UserInfo_API:
+	scope = 'https://www.googleapis.com/auth/userinfo.email'
+	query_url = 'https://www.googleapis.com/oauth2/v2/userinfo'
+
+class AuthConfig:
+	scopes = [Youtube_API.scope, UserInfo_API.scope]
+	auth_url_base = 'https://accounts.google.com/o/oauth2/auth'
+	exchange_request_url = 'accounts.google.com/o/oauth2/token'
+	client_secret_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'client_secret.json')
 
 
 def welcome(request):
@@ -20,7 +28,8 @@ def welcome(request):
 
 def home(request):
 	passed_dict = {}
-	return render_to_response('home.html', passed_dict)
+	passed_dict['user_email'] = oauth.get_current_user(UserInfo_API.scope)
+	return render_to_response('home.html', passed_dic)
 
 def login_page(request):
 	passed_dict = {}
@@ -33,7 +42,7 @@ def login_page(request):
 	client_id = client_secret['web']['client_id']
 	redirect_uri = client_secret['web']['redirect_uris'][0]
 	login_url = '%s?client_id=%s&redirect_uri=%s&scope=%s&response_type=code&' % (
-		AuthConfig.auth_url_base, client_id, redirect_uri, AuthConfig.scope)
+		AuthConfig.auth_url_base, client_id, redirect_uri, ' '.join(AuthConfig.scopes))
 
 	passed_dict['login_url'] = login_url
 	return render_to_response('login.html', passed_dict)
@@ -57,9 +66,18 @@ def exchange_auth(request):
 
 	if auth_api_response.status == 200:
 		auth_api_response = json.loads(auth_api_response.read())
+		## Request Youtube data
 		user_channel_query_url = '%s&access_token=%s' % (Youtube_API.channel_url, auth_api_response['access_token'])
-
-		user_channel_response = urllib.urlopen(user_channel_query_url)
+		user_channel_request = urllib2.Request(user_channel_query_url)
+		user_channel_request.add_header('Authorization', 'Bearer %s' % auth_api_response['access_token'])		
+		user_channel_response = urllib2.urlopen(user_channel_request)
 		user_channel_response = json.loads(user_channel_response.read())
-		
-	return render_to_response('home.html')
+
+		## Request UserInfo data
+		user_info_query_url = '%s?access_token=%s' % (UserInfo_API.query_url, auth_api_response['access_token'])
+		user_info_request = urllib2.Request(user_info_query_url)
+		user_info_request.add_header('Authorization', 'Bearer %s' % auth_api_response['access_token'])		
+		user_info_response = urllib2.urlopen(user_info_request)
+		user_info_response = json.loads(user_info_response.read())
+
+	return redirect('/home')
