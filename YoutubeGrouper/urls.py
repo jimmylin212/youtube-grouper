8b33aef6f391a3321bee36b93732e3af1dc1b25e
jimmylin212 <jimmylin212@gmail.com>
2015-09-12 09:35:46 +0800

    using oauth2client to authencate
    using oauth2client to authencate

--- YoutubeGrouper/urls.py
+++ YoutubeGrouper/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^home/$', 'mainsite.views.home'),
-    url(r'^login/$', 'mainsite.views.login_page'),
-    url(r'^exchange_auth/$', 'mainsite.views.exchange_auth'),
+    url(r'^login/$', 'mainsite.views.login'),
+    url(r'^authencation/$', 'mainsite.views.oauth2callback'),
 )
--- mainsite/views.py
+++ mainsite/views.py
@@ -2,8 +2,11 @@ import json, os, urllib, httplib, urllib2
 
 from django import http
 from django.shortcuts import render_to_response, redirect
-
 from google.appengine.api import oauth
+from oauth2client import client
+from oauth2client.django_orm import Storage
+from apiclient.discovery import build
+from mainsite.models import CredentialsModel
 
 class Youtube_API:
 	scope = 'https://www.googleapis.com/auth/youtube'
@@ -22,62 +25,38 @@ class AuthConfig:
 	client_secret_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'client_secret.json')
 
 
-def welcome(request):
-	passed_dict = {}
-	return render_to_response('home.html', passed_dict) 
-
-def home(request):
-	passed_dict = {}
-	passed_dict['user_email'] = oauth.get_current_user(UserInfo_API.scope)
-	return render_to_response('home.html', passed_dic)
-
-def login_page(request):
-	passed_dict = {}
-
+def oauth2callback(request):
 	## Read client secret file	
 	with open(AuthConfig.client_secret_file, 'rb') as read_client_secret_file:
 		client_secret = json.load(read_client_secret_file)
 
-	## Construct login URL	
-	client_id = client_secret['web']['client_id']
-	redirect_uri = client_secret['web']['redirect_uris'][0]
-	login_url = '%s?client_id=%s&redirect_uri=%s&scope=%s&response_type=code&' % (
-		AuthConfig.auth_url_base, client_id, redirect_uri, ' '.join(AuthConfig.scopes))
+	flow = client.flow_from_clientsecrets(
+		AuthConfig.client_secret_file,
+		scope=AuthConfig.scopes,
+		redirect_uri=client_secret['web']['redirect_uri'][0]
+		)
 
-	passed_dict['login_url'] = login_url
-	return render_to_response('login.html', passed_dict)
+	if request.GET.get('code') == '':
+		auth_uri = flow.step1_get_authorize_url()
+		return redirect(auth_uri)
+	else:
+		code = request.GET.get('code')
+		credentials = flow.step2_exchange(auth_code)
+		storage = Storage(CredentialsModel, 'id', 'TestingUser', 'credential')
+		storage.put(credential)
+		return redirect(r'/home')
 
-def exchange_auth(request):
-	## Read client secret file
-	with open(AuthConfig.client_secret_file, 'rb') as read_client_secret_file:
-		client_secret = json.load(read_client_secret_file)
+def home(request):
+	passed_dict = {}
 
-	code = request.GET.get('code')
-	params = {'code' : code, 
-			  'client_id' : client_secret['web']['client_id'],
-			  'client_secret' : client_secret['web']['client_secret'], 
-			  'redirect_uri' : client_secret['web']['redirect_uris'][0],
-			  'grant_type' : 'authorization_code'
-			  }
+	storage = Storage(CredentialsModel, 'id', 'TestingUser', 'credential')
+	credential = storage.get()
 
-	auth_api_connection = httplib.HTTPSConnection(AuthConfig.exchange_request_url)
-	auth_api_connection.request('POST', '', urllib.urlencode(params))
-	auth_api_response = auth_api_connection.getresponse()
+	passed_dict['user_email'] = oauth.get_current_user(Youtube_API.scope)
+	return render_to_response('home.html', passed_dic)
 
-	if auth_api_response.status == 200:
-		auth_api_response = json.loads(auth_api_response.read())
-		## Request Youtube data
-		user_channel_query_url = '%s&access_token=%s' % (Youtube_API.channel_url, auth_api_response['access_token'])
-		user_channel_request = urllib2.Request(user_channel_query_url)
-		user_channel_request.add_header('Authorization', 'Bearer %s' % auth_api_response['access_token'])		
-		user_channel_response = urllib2.urlopen(user_channel_request)
-		user_channel_response = json.loads(user_channel_response.read())
 
-		## Request UserInfo data
-		user_info_query_url = '%s?access_token=%s' % (UserInfo_API.query_url, auth_api_response['access_token'])
-		user_info_request = urllib2.Request(user_info_query_url)
-		user_info_request.add_header('Authorization', 'Bearer %s' % auth_api_response['access_token'])		
-		user_info_response = urllib2.urlopen(user_info_request)
-		user_info_response = json.loads(user_info_response.read())
+def login(request):
+	passed_dict = {}
+	return render_to_response('login.html', passed_dict) 
 
-	return redirect('/home')
