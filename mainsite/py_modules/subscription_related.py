59fcffca554a02a674549292cdf17d74275eb07d
jimmylin212 <jimmylin212@gmail.com>
2015-11-26 01:06:41 +0800

    use add channel group, parse last 3 days video

--- mainsite/py_modules/subscription_related.py
+++ mainsite/py_modules/subscription_related.py
@@ -1,4 +1,5 @@
-from ..models import UserChannel, Channel
+import datetime, urllib2, re
+from ..models import UserChannel, Channel, Video
 from youtube_related import Youtube
 
 class Subscription:
@@ -17,9 +18,9 @@ class Subscription:
 			search_channel_result = Channel.query(Channel.channel_id == channel_id).get()
 
 			if search_channel_result == None:
-				Channel(channel_id=channel_id, title=channel['title'], thumbnail=channel['thumbnail'], 
+				Channel(channel_id=channel_id, title=channel['title'], thumbnail=channel['thumbnail'],
 						groups=None, upload_playlist_id=None, latest_video_id=None).put()
-				
+
 
 	def get_channel_groups(self, email):
 		channel_groups = {}
@@ -55,9 +56,9 @@ class Subscription:
 		for select_channel_id in select_channel_ids:
 			for group_name in group_names.split(','):
 				## Add the group name into UserChannel collection
-				search_user_channel_result = UserChannel.query(UserChannel.email == email, 
+				search_user_channel_result = UserChannel.query(UserChannel.email == email,
 															   UserChannel.channel_id == select_channel_id).get()
-				
+
 				if search_user_channel_result.group_name == None:
 					search_user_channel_result.group_name = [group_name]
 					search_user_channel_result.put()
@@ -72,13 +73,49 @@ class Subscription:
 				channel_details = youtube_related.get_channel_details(channel_id=select_channel_id, email=email)
 				upload_playlist_id = channel_details['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
+				## If there is no group for the channel, then add. If there is group, then append
 				if search_channel_result.groups == None:
 					search_channel_result.groups = [group_name]
 					search_channel_result.upload_playlist_id = upload_playlist_id
 					search_channel_result.put()
-					
+					## Get the latest uploaded video
+					self.parse_latest_videos(select_channel_id)
+
 				elif group_name not in search_channel_result.groups:
 					search_channel_result.groups.append(group_name)
 					search_channel_result.upload_playlist_id = upload_playlist_id
 					search_channel_result.put()
 
+	def parse_latest_videos(self, channel_id):
+		add_videos = []
+		day_delta = 4
+		feed_url_prefix = r'https://www.youtube.com/feeds/videos.xml?channel_id='
+		day_stop = (datetime.date.today() - datetime.timedelta(days=day_delta)).strftime('%Y-%m-%d')
+
+		entry_ptn = re.compile('\<entry\>(.*?)\<\/entry\>', re.DOTALL)
+		video_id_ptn = re.compile('\<yt\:videoId\>(.*?)\<\/yt\:videoId\>')
+		title_ptn = re.compile('\<title\>(.*?)\<\/title\>')
+		upload_date_ptn = re.compile('\<published\>(.*?)\<\/published\>')
+		thumbnail_ptn = re.compile('media\:thumbnail url\=\"(.*?)\"')
+
+		feed_url = '%s%s' % (feed_url_prefix, channel_id)
+		page = urllib2.urlopen(feed_url)
+		page_source = page.read()
+		entries = entry_ptn.findall(page_source)
+
+		for entry in entries:
+			video_id = video_id_ptn.findall(entry)[0]
+			title = title_ptn.findall(entry)[0]
+			thumbnail = thumbnail_ptn.findall(entry)[0]
+			upload_date = upload_date_ptn.findall(entry)[0].split('T')[0]
+
+			if upload_date == day_stop:
+				break
+
+			if video_id != None and title != None and upload_date != None:
+				upload_date = datetime.datetime.strptime(upload_date, '%Y-%m-%d')
+				## Store new uploaded video
+				store_doc = Video(channel_id=channel_id, title=title, video_id=video_id,
+								  upload_date=upload_date, thumbnail=thumbnail)
+				store_doc.put()
+				add_videos.append(video_id)
